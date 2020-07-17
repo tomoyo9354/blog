6080f86de922e60d410079751063d48a40a8b465
tomoyo9354 <935470672@qq.com>
2020-07-17 13:21:20 +0800

    管理页中owner设为当前用户

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -9,11 +9,19 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name','status','is_nav','create_time')
     fields = ('name','status','is_nav')
 
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.User
+        return super(CategoryAdmin,self).save_model(request,obj,form,change)
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name','status','create_time')
     fields = ('name', 'status')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.User
+        return super(TagAdmin,self).save_model(request,obj,form,change)
 
 
 
