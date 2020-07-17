d82b4676496360b5526aed256eadb72838785358
tomoyo9354 <935470672@qq.com>
2020-07-17 14:41:16 +0800

    admin增加post

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
+from django.utils.html import format_html
 
 from .models import Post,Category,Tag
 # Register your models here.
@@ -24,4 +26,36 @@ class TagAdmin(admin.ModelAdmin):
         return super(TagAdmin,self).save_model(request,obj,form,change)
 
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title','category','status',
+        'create_time','operator'
+    ]
+    list_display_links = []
+
+    list_filter = ['category',]
+    search_fields = ['title','category__name']
+
+    actions_on_top = True
+    actions_on_bottom = True
+
+    save_on_top = True
+
+    fields = (
+        ('category','title'),
+        'desc',
+        'status',
+        'content',
+        'tag',
+    )
+
+    def operator(self,obj):
+        return format_html('<a href="{}">编辑</a>',reverse('admin:blog_app_post_change',args=(obj.id,)))
+
+    operator.short_description = '操作'
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(PostAdmin,self).save_model(request,obj,form,change)
 
