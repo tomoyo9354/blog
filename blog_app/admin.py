4c1b9a45c1649e0f30accf13ac6921ed179c89bf
tomoyo9354 <935470672@qq.com>
2020-07-17 11:17:48 +0800

    blog_app的admin第1次提交

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import Post,Category,Tag
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','status','is_nav','create_time')
+    fields = ('name','status','is_nav')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name','status','create_time')
+    fields = ('name', 'status')
+
+
+
