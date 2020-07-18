53983fff54b7c9ff13c3f9630f580fc2706a8b6b
tomoyo9354 <935470672@qq.com>
2020-07-18 14:15:02 +0800

    定制site

--- blog/urls.py
+++ blog/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from .custom_site import custom_site
+
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', custom_site.urls),
+    url(r'^super_admin/',admin.site.urls),
 ]
