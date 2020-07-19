3cc44f1d99e348392fe60f45604a2c5cd62a7e28
tomoyo9354 <935470672@qq.com>
2020-07-19 10:04:49 +0800

    提取admin基类

--- comment/admin.py
+++ comment/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
 from .models import Comment
+from blog.base_admin import BaseOwnerAdmin
 # Register your models here.
 
 
 @admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+class CommentAdmin(BaseOwnerAdmin):
     list_display = ('target','nickname','content','website','created_time')
 
 
