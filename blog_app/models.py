11e56bd2ac8d05317185bd8e14c6df339951026a
tomoyo9354 <935470672@qq.com>
2020-07-22 22:40:14 +0800

    侧边栏显示

--- blog_app/models.py
+++ blog_app/models.py
@@ -82,6 +82,8 @@ class Post(models.Model):
     tag = models.ManyToManyField(Tag,verbose_name='标签')
     owner = models.ForeignKey(User,verbose_name='作者')
     create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    pv = models.PositiveIntegerField(default=1)
+    uv = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return self.title
@@ -112,6 +114,10 @@ class Post(models.Model):
     def latest_posts(cls):
         queryset = cls.objects.filter(status=cls.STATUS_NORMAL)
         return queryset
+
+    @classmethod
+    def hot_posts(cls):
+        return cls.objects.filter(status=cls.STATUS_NORMAL).order_by('-pv')
 
     class Meta:
         verbose_name = '文章'
